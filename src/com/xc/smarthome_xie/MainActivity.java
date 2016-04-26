f83adec6ac389c39ae09f3265f5041ec0769a9bd
chorm590 <yang662886@qq.com>
2016-04-26 22:11:46 +0800

    create mainActivity,but no main layout.

---
+++ src/com/xc/smarthome_xie/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xc.smarthome_xie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+}
